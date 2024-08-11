fa54b5d2770fc9b05ea7d47d2b675fc0e145f3eb
devcenourinha <gabrielbuenotl2@gmail.com>
2024-08-11 19:22:35 -0300

    meus testes de commit apos alterar a branch para main

--- meu primeiro projeto Java/src/App.java
+++ meu primeiro projeto Java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World this is my firs project");
+        System.out.println("Hello, World this is my TEST");
     }
 }
