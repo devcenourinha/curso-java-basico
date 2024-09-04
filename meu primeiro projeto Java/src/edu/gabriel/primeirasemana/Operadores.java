30b7c2553af8544bff435158ef601dbd5af191f1
devcenourinha <gabrielbuenotl2@gmail.com>
2024-09-03 23:18:14 -0300

    incremento numerico e expressoes boleanas

--- meu primeiro projeto Java/src/edu/gabriel/primeirasemana/Operadores.java
+++ meu primeiro projeto Java/src/edu/gabriel/primeirasemana/Operadores.java
@@ -2,6 +2,7 @@ package edu.gabriel.primeirasemana;
 
 public class Operadores {
 
+    //x tipos variaveis
     String nome = "Gabriel";
     int idade = 21;
     double peso = 63.5;
@@ -9,12 +10,27 @@ public class Operadores {
     boolean doadorOrgao = false;
     Date dataNascimento = new Date() ;
 
+    //x tipos operações
     double soma = 10.5 + 15.7;
     int subtração = 113 - 25;
     int multiplicacao = 20 * 7;
     int divisao = 15 / 3;
     int modulo = 18 % 3;
     double resultado = (10 * 7) + (20/4);
+
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        numero = numero + 2;
+        System.out.println(numero);
+    }
+
+    public static void main(String[] args) {
+        
+        boolean variavel = true;
+        variavel = !variavel;
+        System.out.println(variavel);
+    }
 
 }
 
