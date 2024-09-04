6df530d031ef44fca395e24e898793a9fcb01005
devcenourinha <gabrielbuenotl2@gmail.com>
2024-09-03 22:58:56 -0300

    aula operadores

---
+++ meu primeiro projeto Java/src/edu/gabriel/primeirasemana/Operadores.java
@@ -0,0 +1,20 @@
+package edu.gabriel.primeirasemana;
+
+public class Operadores {
+
+    String nome = "Gabriel";
+    int idade = 21;
+    double peso = 63.5;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    Date dataNascimento = new Date() ;
+
+    double soma = 10.5 + 15.7;
+    int subtração = 113 - 25;
+    int multiplicacao = 20 * 7;
+    int divisao = 15 / 3;
+    int modulo = 18 % 3;
+    double resultado = (10 * 7) + (20/4);
+
+}
+
